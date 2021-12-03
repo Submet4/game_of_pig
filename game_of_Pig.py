615b6255854d4cade8fc7971233c192037d55532
Submet4 <91331513+Submet4@users.noreply.github.com>
2021-12-03 16:59:00 +0100

    first commit

---
+++ game_of_Pig.py
@@ -0,0 +1,10 @@
+#Spielerklärung:
+#Gewürfelt wird reihum mit zwei Würfeln. Es wird immer die geworfene Augensumme addiert. Jeder Spieler darf so oft würfeln, wie er möchte.
+#Der Spieler, der zuerst 100 Punkte hat, ist Sieger.
+#Wird allerdings eine 1 geworfen, erhält der Spieler 0 Punkte in der Runde und muss die Würfel weitergeben.
+#Werden sogar zwei Einsen
+#geworfen, wird die Gesamtpunktzahl auf 0 zurückgesetzt und der Würfel weitergegeben.
+#
+
+#Test
+
