f051662415c2659d63a3462855f7769966e505de
Submet4 <91331513+Submet4@users.noreply.github.com>
2022-01-02 13:33:55 +0100

    lets start this properly

--- game_of_Pig.py
+++ game_of_Pig.py
@@ -6,5 +6,15 @@
 #geworfen, wird die Gesamtpunktzahl auf 0 zurückgesetzt und der Würfel weitergegeben.
 #
 
-#Test
+#Initialisierung
 
+player1_score = 0
+player2_score = 0
+
+goal_score = 30
+
+ongoing_round_score = 5
+
+player1_score = player1_score + ongoing_round_score
+
+print(player1_score)
